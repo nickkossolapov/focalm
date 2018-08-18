a0860a3b509b7c5085fa71389c4f0042eaf338e6
Nick <nickkossolapov@gmail.com>
2018-08-18 19:30:20 +0200

    Added deployment config

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import os
 from src.app import create_app
 
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
+
 if __name__ == '__main__':
-    env_name = os.getenv('FLASK_ENV')
-    app = create_app(env_name)
-    app.run(port=443)
+    port = os.getenv('PORT')
+    app.run(host='0.0.0.0', port=port)
