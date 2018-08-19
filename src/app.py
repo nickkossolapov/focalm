9dde60295be60cef838f60173c1e4f6bac2c6c88
Nick <nickkossolapov@gmail.com>
2018-08-19 11:12:18 +0200

    Added demo React app with setup

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from .config import app_config
 from .models import db, bcrypt
@@ -6,11 +6,7 @@ from .models import db, bcrypt
 from .controllers.UserController import user_api as user_blueprint
 
 def create_app(env_name):
-    """
-    Create app
-    """
-
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../static', template_folder="../static")
     app.config.from_object(app_config[env_name])
 
     bcrypt.init_app(app)
@@ -20,9 +16,6 @@ def create_app(env_name):
 
     @app.route('/', methods=['GET'])
     def index():
-        """
-        example endpoint
-        """
-        return 'Congratulations! Your first endpoint is working'
+        return render_template('index.html')
 
     return app
