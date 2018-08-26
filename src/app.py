68bec47576133040d451fae5073ebf7568b4d0d2
Nick <nickkossolapov@gmail.com>
2018-08-26 17:54:47 +0200

    Added decorator to prevent caching of static files when in dev mode

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+import datetime
+from functools import wraps, update_wrapper
+
+from flask import Flask, render_template, make_response
 
 from .config import app_config
 from .models import db, bcrypt
@@ -17,5 +20,19 @@ def create_app(env_name):
     @app.route('/', methods=['GET'])
     def index():
         return render_template('index.html')
+
+    @app.after_request
+    def add_header(r):
+        """
+        Add headers to both force latest IE rendering engine or Chrome Frame,
+        and also to cache the rendered page for 10 minutes.
+        https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
+        """
+        if env_name == "development":
+            r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+            r.headers["Pragma"] = "no-cache"
+            r.headers["Expires"] = "0"
+            r.headers['Cache-Control'] = 'public, max-age=0'
+        return r
 
     return app
