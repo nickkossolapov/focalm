2edd93ddd5897adfc5373b57dc30e60284e041f3
Nick <nickkossolapov@gmail.com>
2018-09-08 19:56:04 +0200

    Add endpoint for calendar meals

--- src/models/CalendarMealModel.py
+++ src/models/CalendarMealModel.py
@@ -1,13 +1,13 @@
 import datetime
-from enum import Enum
-
+from enum import IntEnum
 from marshmallow import fields, Schema
+from marshmallow_enum import EnumField
 from sqlalchemy.orm import relationship
 
 from . import db
 
 
-class MealTime(Enum):
+class MealTime(IntEnum):
     BREAKFAST = 1,
     LUNCH = 2,
     DINNER = 3
@@ -19,16 +19,15 @@ class CalendarMealModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     meal_id = db.Column(db.Integer, db.ForeignKey('meals.id'), nullable=False)
-    meal = relationship('MealModel')
-    meal_time = db.Column(db.String(16), Enum(MealTime),nullable=False)
-    meal_date = db.Column(db.DateTime, nullable=False)
+    meal_time = db.Column(db.Integer, nullable=False)
+    meal_date = db.Column(db.Date, nullable=False)
 
     def __init__(self, data, user_id):
         self.name = data.get('name')
-        self.user_id = data.get('user_id')
-        self.servings = data.get('servings')
-        self.meal_time = data.get('meal_time')
-        self.meal_date = datetime.datetime.utcnow()
+        self.user_id = user_id
+        self.meal_id = data.get('meal_id')
+        self.meal_time = int(data.get('time'))
+        self.meal_date = data.get('date')
 
     def save(self):
         db.session.add(self)
@@ -49,7 +48,7 @@ class CalendarMealModel(db.Model):
 
 class CalendarMealSchema(Schema):
     id = fields.Int(dump_only=True)
-    user_id = fields.Int(required=True)
+    user_id = fields.Int(dump_only=True)
     meal_id = fields.Int(required=True)
-    meal_time = fields.DateTime(required=True)
-    meal_date = fields.DateTime(required=True)
+    time = EnumField(MealTime, required=True)
+    date = fields.Date(required=True)
--- src/views/CalendarMealView.py
+++ src/views/CalendarMealView.py
@@ -5,8 +5,22 @@ from ..models.CalendarMealModel import CalendarMealModel, CalendarMealSchema
 from ..shared.Authenticiation import Auth
 
 
-def create_ingredient_view():
+def create_calender_meal_view():
     calendar_meal_api = Blueprint('meal_calendar_api', __name__)
     calendar_meal_schema = CalendarMealSchema()
+
+    @calendar_meal_api.route('/', methods=['POST'])
+    @Auth.auth_required
+    def create():
+        try:
+            req_data = request.get_json()
+            data = calendar_meal_schema.load(req_data)
+
+            calendar_meal = CalendarMealModel(data, g.user.get('id'))
+            calendar_meal.save()
+
+            return Response(status=201)
+        except ValidationError as err:
+            return Response(status=400)
 
     return calendar_meal_api
