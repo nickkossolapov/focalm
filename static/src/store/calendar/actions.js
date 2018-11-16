39c9ac6bd82cd8f83671b8e1653f2353268768a1
Nick Kossolapov <nick.kossolapov@entelect.co.za>
2018-11-16 10:11:34 +0200

    WIP: Add saving of calendar meals to DB

--- static/src/store/calendar/actions.js
+++ static/src/store/calendar/actions.js
@@ -1,11 +1,25 @@
 import {ADD_DAY_ITEM} from "./types";
+import {getApiPostRequest} from "../helpers/api_helpers";
+import axios from "axios";
 
-export const addDayItem = (mealId, dateId) => async (dispatch) =>  {
-  dispatch({
-    type: ADD_DAY_ITEM,
-    payload: {
-      dateId,
-      mealId
-    }
-  });
+const CALENDAR_API = "/api/calendar/";
+
+export const addDayItem = (mealId, dateId) => async (dispatch, getState) =>  {
+  try {
+    const { auth: {authenticated: token} } = getState();
+
+    // const apiRequest = getApiPostRequest(CALENDAR_API, token, );
+    // const response = await axios(apiRequest);
+
+    dispatch({
+      type: ADD_DAY_ITEM,
+      payload: {
+        dateId,
+        mealId
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
 };
